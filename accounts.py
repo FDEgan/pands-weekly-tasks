3a0e204a9c33d41d0336c7767cd1b2254a7899a5
FDEgan <157654218+FDEgan@users.noreply.github.com>
2024-02-05 18:00:34 +0000

    Add files via upload
    Task:
    Weekly Task 03
    
    Background:
    Bank account numbers can stored as 10 character strings, for security reasons some applications only display the last 4 characters (with the other other characters replaced with Xs).
    
    Objective:
    Write a python program called accounts.py that reads in a 10 character account number and outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs).
    
    Extra:
    Modify the program to deal with account numbers of any length (yes that is a vague requirement, comment your assumptions)

---
+++ accounts.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+account_number = '53770623187116811810' # Setting an account number as a string
+
+
+# In[29]:
+
+
+num_leading_digits = len(account_number)-4 # Creating a variable to calculate number of leading digits before the last 4 digits
+
+
+# In[30]:
+
+
+print(num_leading_digits) # printing the number of leading digits for validation
+
+
+# In[33]:
+
+
+four_digits = account_number[-4:] # Creating variable to identify 4 ending digits of account number
+
+
+# In[34]:
+
+
+print(four_digits) # Printing the last four digits for validation
+
+
+# In[35]:
+
+
+lead_x = num_leading_digits * "X" # Creating variable to mask the number of leading digits with 'X'
+
+
+# In[36]:
+
+
+print(lead_x) # Printing the number of X's for validation
+
+
+# In[37]:
+
+
+encrypted_acc_num = lead_x + four_digits # creating the encrypted account number by adding the lead_x and four_digits
+
+
+# In[38]:
+
+
+print(encrypted_acc_num) # Printing the calculated variable for validation
+
+
+# In[ ]:
+
+
+
+
