af1837713d0b509f5d9b14bd318066c7bd5e8761
Barry Egan <barryegan1991@hotmail.com>
2024-02-18 16:48:12 +0000

    Updated commit with comments included and a new name for the list

---
+++ collatz.py
@@ -0,0 +1,10 @@
+int = 10 #Setting my initial list
+
+num_list = [int] #Creating a new list that will take in the integer I chose
+while int != 1: # Using a while to dictate, when the value is not 1 to persist through the loop
+    if int % 2 == 0: # Using a modulo to see if the value is odd or even - This is in case the value is even
+        int = int // 2 # When value is even, divide the value by 2
+    else: # In case the value is not even
+        int = 3 * int + 1 # Logic for when the value is odd. Multiply by 3 and add 1
+    num_list.append(int) #append the value to the new list I created
+print(num_list) # Print out that new list
