5a6b922413f1c648a974022247ae7357af5cad78
Barry Egan <barryegan1991@hotmail.com>
2024-04-01 14:50:41 +0100

    Week 8 Task

---
+++ plottask.py
@@ -0,0 +1,16 @@
+# Importing Libraries Required for the task
+import numpy as np
+import matplotlib.pyplot as plt 
+plot_data = np.random.normal(5, 2, 1000) # First Argument is Mean = 5/ Second Argument is Std Dev = 2 and Third Argument is the Size of the Array
+#Creating Values for 2nd Plot X Axis.
+x = np.arange(0, 10.1, 0.1)  #1st Argument = Start of the Values/ 2nd = End of the Values/ 3rd Step Size of the values
+y = x ** 3 # Creating the Y Values based on the function provided.
+plt.figure(figsize=(20, 10)) # Dictating the Width & Height in Inches
+plt.hist(plot_data, bins=15, color='green', label= 'Histogram Plot') # Creating the Histrogram/ 1st Arg = Data to Plot/ 2nd Arg Number of Bins in Graph/ 3rd Color Of Hist & Naming the plot 
+plt.plot(x, y, color='yellow', label='Function Plot') #Plotting the 2nd Function, setting color & Naming the Plot)
+plt.title('Histogram of Normal Distribution for Week 8 Task') #Setting Title of the Figure
+plt.xlabel('Value') #Setting Label of X Axis
+plt.ylabel('No. of Occurrences Of Values') #Setting Label of Y Axis
+plt.legend() # Showing the legend
+plt.show() # Showing the plot
+# Used for 2nd Function: http://www.ece.northwestern.edu/local-apps/matlabhelp/techdoc/ref/linspace.html#:~:text=linspace%20(MATLAB%20Functions)&text=The%20linspace%20function%20generates%20linearly,and%20including%20a%20and%20b.
